d6391ebfa7e3632ae1e89d1bfc21d5676febf50d
Romeo292 <rngobeni168@gmail.com>
2018-09-20 15:49:59 +0200

    romeo added searchbar

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,28 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  
+  public isSearchbarOpened=false;
+
+  topics=[];
 
   constructor(public navCtrl: NavController) {
-
+     this.generateTopics();
+  }
+  generateTopics(){
+    this.topics=["Dj clock","DJ balck Coffee","Dj ganyani"];
+  }
+  getTopics(ev:any){
+     this.generateTopics();
+     let serVal=ev.target.value;
+      if(serVal && serVal.trim() !=''){
+         this.topics=this.topics.filter((topic)=>{
+           return (topic.toLowerCase().indexOf(serVal.toLowerCase))>-1
+         });
+      }
   }
 
+  onSearch(event){
+    
+  }
 }
