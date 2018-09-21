1db8f78996b1cc85047daa32aaa587ee7555d1d4
Precia <wprecia4@gmail.com>
2018-09-21 14:57:06 +0200

    login

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,6 +1,10 @@
+import { AccountProvider } from './../../providers/account/account';
+import { HomePage } from './../home/home';
+import { AuthProvider } from './../../providers/auth/auth';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { Alert,Loading, LoadingController, AlertController, ToastController } from 'ionic-angular';
+import firebase from 'firebase/app';
 /**
  * Generated class for the SignupPage page.
  *
@@ -14,12 +18,72 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'signup.html',
 })
 export class SignupPage {
+  
+  Firstname:string;
+  Lastname:string;
+  Email:string;          //declaration
+  Password:string;
+  Confirm:string;
+  data=[];
+  load:Loading;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  
+   constructor(private navCtrl: NavController,private accountP:AccountProvider, private loadingCtrl: LoadingController,
+    private alertCtrl:AlertController, private provider:AuthProvider, private toastCtrl: ToastController) {
+ 
+    }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SignupPage');
   }
 
+
+  submit(){
+  var account={
+  Email:this.Email,      
+  Password:this.Password,
+  };
+
+  var loader = this.loadingCtrl.create({
+    content: "Please wait...",
+    
+  });
+  loader.present();
+
+this.provider.signupUser(account).then(authData=>{
+  loader.dismiss();
+// this.load.dismiss().then(()=>{
+    // this.navCtrl.setRoot(HomePage);
+  // })
+ },error=>{
+   loader.dismiss();
+
+   let toast= this.toastCtrl.create({
+    message: error,
+    duration: 3000,
+    position: 'top'
+
+   });
+   toast.present();
+   this.Password = ""
+ });
+
+{
+ let alert: Alert =this.alertCtrl.create({
+  message:"Congratulations",
+  buttons:[{
+    text:'OK',
+    handler:data=>{
+    this.navCtrl.setRoot(HomePage)
+    }
+
+  }]
+ })
+ alert.present();
 }
+this.accountP.updateDetails(this.Firstname,this.Lastname,this.Email) 
+
+}  //method
+
+
+} //export
--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -1,7 +1,16 @@
+<<<<<<< HEAD
 import { Injectable } from '@angular/core';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database'; 
+=======
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Injectable } from '@angular/core';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+import 'rxjs/add/operator/map';
+>>>>>>> 5760df26b3ec7779428728886e4761ac7a21873a
 
 /*
   Generated class for the AuthProvider provider.
@@ -11,7 +20,20 @@ import 'firebase/database';
 */
 @Injectable()
 export class AuthProvider {
+<<<<<<< HEAD
   constructor() {
+=======
+   userProfile:firebase.database.Reference;
+  data: any;
+  fireAuth: any;
+  
+  
+  constructor() {
+    this.fireAuth = firebase.auth();
+    this.userProfile = firebase.database().ref('users');
+
+    console.log('Hello AuthProvider Provider');
+>>>>>>> 5760df26b3ec7779428728886e4761ac7a21873a
   }
 signIn(email:string,password:string):Promise<any>{
   return firebase.auth().signInWithEmailAndPassword(email,password);
@@ -20,5 +42,25 @@ signIn(email:string,password:string):Promise<any>{
 resetPassword(email:string):Promise<any>{
   return firebase.auth().sendPasswordResetEmail(email);
 }
+
+
+  
+
+   signupUser(account:{}){
+
+    
+     return this.fireAuth.createUserWithEmailAndPassword(account['Email'], account['Password']).then((newUser) => {
+       //sign in the user
+        this.fireAuth.signInWithEmailAndPassword(account['Email'], account['Password']).then((authenticatedUser) => {
+         //successful login, create user profile
+       this.userProfile.child( authenticatedUser.uid).set(
+         account
+       );
+       });
+      });
+
+}
+
+     
 
 }
