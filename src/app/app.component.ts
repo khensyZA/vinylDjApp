152d37b958bba925dd526e0ab7a33297c053e497
Romeo292 <rngobeni168@gmail.com>
2018-09-21 13:18:37 +0200

    Romeo is checking the sidemenu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ import { HomePage } from './../pages/home/home';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = HomePage;
+  rootPage: any = OnboardingPage;
 
   pages: Array<{title: string, component: any}>;
 
